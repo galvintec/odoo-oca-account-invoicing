40ff4f5ab0e05de05d7160b930197aae36273775
Gabriel Nogueira <47315506+gabonog@users.noreply.github.com>
2024-03-01 08:37:55 +0100

    [FIX] Add future_fstrings on top of python file

--- account_invoice_default_code_column/models/account_invoice_line.py
+++ account_invoice_default_code_column/models/account_invoice_line.py
@@ -1,3 +1,4 @@
+# -*- coding: future_fstrings -*-
 # Copyright (C) 2023 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
